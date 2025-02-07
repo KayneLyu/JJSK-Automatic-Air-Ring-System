172411720f3bb4f239c124a4bebf7aac69e60dd7
Kayne Lyu <kangzaiwt@gmail.com>
2025-02-07 15:27:06 +0800

    Update AreaCharts.vue
    测试

--- src/views/horizontal/AreaCharts.vue
+++ src/views/horizontal/AreaCharts.vue
@@ -168,6 +168,7 @@ const { updateCharts } = useChartsInit('chartContainer', option)
 //             data: [1000, 1000, 901, 934, 1290, 1330, 1320],
 //             type: 'line',
 //             areaStyle: {}
+
 //         }
 //     ]
 //     })
